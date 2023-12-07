05e0292da402d6809ae72159ac2392094fa0da00
Fenix <136452367+Spyfenix@users.noreply.github.com>
2023-12-07 15:16:15 -0300

    Update 07/12/2023
    Atualização Final

--- index.js
+++ index.js
@@ -4,3 +4,38 @@ faqs.forEach(faq =>{
         faq.classList.toggle("active")
     })
 })
+/* CODIGO ACIMA TRATA SOBRE O MENU*/
+let mais = document.getElementById('mais')
+let btnLeiaMais = document.getElementById('btnLeiaMais')
+let textoAparecendo = false;
+
+function alteraEstado(para, textoDentro, estadoDisplay){
+    mais.style.display = estadoDisplay;
+    btnLeiaMais.innerText = textoDentro;
+    textoAparecendo = para;
+}
+btnLeiaMais.addEventListener('click', () => {
+    if(textoAparecendo){
+        alteraEstado(false, 'Leia mais', 'none');
+        return;
+    }
+    alteraEstado(true, 'Leia menos', 'flex');    
+})
+/* codigo acima trata-se sobre o botao ler mais */
+
+let mybutton = document.getElementById("myBtn");    
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+} 
